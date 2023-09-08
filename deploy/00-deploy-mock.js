4a434702421d8e53955e50a96858c4055d6de16f
Tarun Rao <tarunsanjayrao@gmail.com>
2023-09-08 19:52:06 +0530

    test

--- deploy/00-deploy-mock.js
+++ deploy/00-deploy-mock.js
@@ -1,8 +1,8 @@
 const { network } = require("hardhat")
 const { DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
 
-const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
-const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+const BASE_FEE = "250000000000000000" 
+const GAS_PRICE_LINK = 1e9 
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
--- test/unit/basicNft.test.js
+++ test/unit/basicNft.test.js
@@ -4,8 +4,6 @@ const { assert } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
 const { developmentChains } = require("../../helper-hardhat-config")
 
-//writing the test code from here..
-
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Basic NFT Unit Tests", function () {
